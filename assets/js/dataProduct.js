ff29e000b217b9663dc5c691a542b472055af1c2
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-11-02 00:00:10 +0700

    lọc sp theo brand, pagination

--- assets/js/dataProduct.js
+++ assets/js/dataProduct.js
@@ -396,13 +396,13 @@ const productArr = [
         20
     ),
     new Product(
-        "realme C30s",
-        2690000.0,
-        2390000.0,
-        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/r/e/realme-c30s-den-glr--009_3_3.jpg'],
-        "Samsung",
-        ["3 GB"],
-        ["64 GB"],
+        "realme GT Neo 3",
+        6950000.0,
+        4550000.0,
+        ['https://cdn.mobilecity.vn/mobilecity-vn/images/2022/06/w300/realme-gt-neo-3-xanh-tim-1.jpg.webp'],
+        "Realme",
+        ["6 GB","8 GB","12 GB"],
+        ["128 GB","256 GB"],
         11
     ),
     new Product(
@@ -426,14 +426,14 @@ const productArr = [
         5
     ),
     new Product(
-        "Masstel Lux 10",
-        530000.0,
-        600000.0,
-        ['https://cdn.tgdd.vn/Products/Images/42/299611/masstel-lux-10-vang-thumb-600x600.jpg'],
-        "Masstel",
+        "Vivo Y01",
+        2390000.0,
+        2090000.0,
+        ['https://cdn.tgdd.vn/Products/Images/42/268880/vivi-y01-%C4%91en-thumb-600x600.jpg'],
+        "Vivo",
         ["1 GB"],
         ["16 GB"],
-        33
+        12
     ),
     new Product(
         "Vivo V25 Pro 5G",
@@ -455,7 +455,157 @@ const productArr = [
         ["256 GB"],
         11
     ),
-    
+    new Product(
+        "iPhone 15 Pro Max 256GB",
+        34190000.0,
+        32190000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-15-pro-max_3.png'],
+        "iPhone",
+        ["256GB"],
+        ["512GB","1TB"],
+        2
+    ),
+    new Product(
+        "iPhone 14 Pro Max 128GB",
+        29900000.0,
+        24790000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-14-pro_2__5.png'],
+        "iPhone",
+        ["128GB","256GB"],
+        ["512GB","1TB"],
+        11
+    ),
+    new Product(
+        "iPhone 15 128GB ",
+        22990000.0,
+        20490000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-15-plus_1__1.png'],
+        "iPhone",
+        ["128GB"],
+        ["256GB","512GB"],
+        4
+    ),
+    new Product(
+        "iPhone 13 128GB",
+        18990000.0,
+        15250000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-13_2_.png'],
+        "iPhone",
+        ["128GB"],
+        ["256GB","512GB"],
+        14
+    ),
+    new Product(
+        "iPhone 15 Pro Max 512GB",
+        40990000.0,
+        37790000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-15-pro-max_5.png'],
+        "iPhone",
+        ["512GB"],
+        ["1TB"],
+        3
+    ),
+    new Product(
+        "iPhone 14 Pro 128GB",
+        27990000.0,
+        22190000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-14-pro_2__4.png'],
+        "iPhone",
+        ["128GB","256GB"],
+        ["512GB","1TB"],
+        ["iPhone"],
+        14
+    ),
+    new Product(
+        "iPhone 15 Plus 128GB",
+        25990000.0,
+        24490000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-15-plus_1_.png'],
+        "iPhone",
+        ["128GB"],
+        ["256GB","512GB"],
+        0
+    ),
+    new Product(
+        "iPhone 14 128GB",
+        22900000.0,
+        16790000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-14_1.png'],
+        "iPhone",
+        ["128GB"],
+        ["256GB","512GB"],
+        18
+    ),
+    new Product(
+        "iPhone 13 Pro Max 128GB",
+        28900000.0,
+        22990000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-13-pro-max.png'],
+        "iPhone",
+        ["128GB","256GB"],
+        ["512GB","1TB"],
+        17
+    ),
+    new Product(
+        "iPhone 12 64GB",
+        15990000.0,
+        12390000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-12.png'],
+        "iPhone",
+        ["64GB","128GB"],
+        ["256GB"],
+        16
+    ),
+    new Product(
+        "OPPO Reno8 T 4G 256GB",
+        8490000.0,
+        6590000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/o/p/oppo-reno-8t-4g-256gb.png'],
+        "OPPO",
+        ["8G"],
+        ["256GB"],
+        16
+    ),
+    new Product(
+        "OPPO A58 4G 6GB 128GB",
+        4990000.0,
+        4190000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/o/p/oppo-a58_1_1.png'],
+        "OPPO",
+        ["6GB"],
+        ["128GB"],
+        6
+    ),
+    new Product(
+        "OPPO Reno8 T 5G (8GB - 128GB)",
+        9990000.0,
+        7890000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/o/p/oppo-reno-8t.png'],
+        "OPPO",
+        ["8GB"],
+        ["128GB"],
+        21
+    ),
+    new Product(
+        "OPPO A77s",
+        6290000.0,
+        4550000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/o/p/oppo-a55_1_.png'],
+        "OPPO",
+        ["8GB"],
+        ["128GB"],
+        20
+    ),
+    new Product(
+        "OPPO A55 (4GB-64GB)",
+        3690000.0,
+        2990000.0,
+        ['https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/o/p/oppo-a55_1_1.png'],
+        "OPPO",
+        ["4GB"],
+        ["64GB"],
+        5
+    ),
 
 ]
 
