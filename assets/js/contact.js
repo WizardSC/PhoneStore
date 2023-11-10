283b8fd665221b18545f025f39315f902eaba369
phong1221 <quangphong4321@gmail.com>
2023-11-10 10:04:48 +0700

    contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -8,17 +8,27 @@ function Validator(option)
             var errorElement = inputElement.parentElement.querySelector('.form-message');
             if(inputElement)
             {
+                // xử lí trường hợp khi blur ra khỏi input mà chưa nhập đúng dữ liệu
                 inputElement.onblur = function()
                 {
                     var errorMessage =  rule.test(inputElement.value);
                     if(errorMessage)
                     {
                         errorElement.innerText = errorMessage;
+                        inputElement.parentElement.classList.add('message');
+                        // inputElement.parentElement.classList.add('');
                     }
                     else
                     {
                         errorElement.innerText ='';
+                        inputElement.parentElement.classList.remove('message');
+    
                     }
+                }
+                // xử lí trường hợp khi nhập vào ô input
+                inputElement.oninput = function()
+                {
+                    errorElement.innerHTML = '' ;
                 }
             }
         })
@@ -28,16 +38,53 @@ Validator.isRequire = function(selector) {
     return {
         selector : selector,
         test : function (value) {
-            return value.trim() ? undefined : 'vui long nhap truong nay !!';
+            return value.trim() ? undefined : ' Vui lòng nhập  trường này !!' ;
         }
     };
 }
 Validator.isEmail = function(selector) {
     return {
         selector : selector ,
-        test : function() {
-
+        test : function(value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : 'Vui lòng nhập Email  !! ';
         }
     };
 
 }
+Validator.isPhone = function(selector){
+    return {
+        selector: selector,
+        test: function(value) {
+            var sdt = value.replace(/[^0-9]/g, ''); // Sửa tên biến và cập nhật regex
+            return /^\d{10}$/.test(sdt) ? undefined : 'Vui lòng nhập lại số điện thoại';
+        }
+    };
+};
+Validator.minLength = function(selector){
+    return {
+        selector: selector,
+        test: function(value) {
+            var length = value.length;
+            return (length >= 6 && length <= 30) ? undefined : 'Vui lòng nhập giữa 6 và 30 ký tự';
+        }
+    };
+};
+Validator.isUserName = function(selector){
+    return {
+        selector: selector,
+        test: function(value) {
+            // Kiểm tra xem tên không chứa chữ số và kí tự đặc biệt
+            if(/\d/.test(value)) {
+                return 'Vui lòng không nhập chữ số trong tên';
+            } else if(/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
+                return 'Vui lòng không nhập kí tự đặc biệt trong tên';
+            } else if(value.length > 30) {
+                return 'Tên không được quá 30 ký tự';
+            } else {
+                return undefined; // Không có lỗi
+            }
+        }
+    };
+};
+
