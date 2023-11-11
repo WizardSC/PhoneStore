91ee868c433001e315e7f1ce0e95957aded1c6d8
Trong-123 <trongngo.02092004@gmail.com>
2023-11-11 17:59:52 +0700

    aaaa

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -8,8 +8,26 @@ login.addEventListener('click',function(e){
     e.preventDefault()
     if(login.classList.contains('active1')){
         user.classList.add('active1')
+        login.classList.remove('active1')
     }
 })
+
+const login1=document.querySelector(".login1")
+const user1=document.querySelector(".user1")
+
+console.log(login1)
+console.log(user1)
+
+// login1.addEventListener('click',function(e){
+//     e.preventDefault()
+//     if(login1.classList.contains('active2')){
+//         user1.classList.add('active2')
+//         login1.remove('active2')
+//     }
+// })
+
+
+
 
 
 
