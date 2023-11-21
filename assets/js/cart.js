6ae8046cc14d5869418b47f8b8e3c932a400dcf6
Trong-123 <trongngo.02092004@gmail.com>
2023-11-21 13:13:22 +0700

    aaaa

---
+++ assets/js/cart.js
@@ -0,0 +1,33 @@
+// let openShopping = document.querySelector('.shopping');
+// let closeShopping = document.querySelector('.cart__checkOut-closeShopping');
+// let listCart = document.querySelector('.cart__listCart');
+// let cart = document.querySelector('.cart');
+// let total = document.querySelector('.cart__checkOut-total');
+// let quantity = document.querySelector('.quantity');
+// let listCarts = [];
+
+// console.log(openShopping);
+// console.log(closeShopping);
+// console.log(listCart);
+// console.log(productArr);
+// console.log(cart);
+// console.log(total);
+// console.log(quantity);
+
+// openShopping.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     cart.classList.add('activeCart');
+// });
+
+// closeShopping.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     cart.classList.remove('activeCart');
+// });
+
+// let addToCartButtons = document.querySelectorAll('.product-detail__btn-add-to-cart');
+
+
+ 
+
+
+
